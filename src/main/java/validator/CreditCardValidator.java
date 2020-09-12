01d00555ac35ae6bbb7c80f996efe66092e77b04
Włodek Krakowski <wlodzimierz.krakowski@onet.pl>
2020-09-12 09:31:04 +0200

    Cleanup : - Replace Java 1.4 for loop with stream - Use abstraction instead of implementation (java.util.List) - Remove redundant local variables - Optimize for faster result-retrieval using anyMatch - Optimize for faster result but not investigating all 3 parts of check

--- src/main/java/validator/CreditCardValidator.java
+++ src/main/java/validator/CreditCardValidator.java
@@ -1,62 +1,45 @@
 package validator;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class CreditCardValidator implements CreditCardChecking {
 
 	@Override
 	public boolean checkValidity(CreditCardVendor ccv, String number) {
-		ArrayList<Integer> chars = ccv.getNumberOfChars();
-		ArrayList<String> masks = ccv.getIIN();
-		
-		boolean result1 = checkLengthCorrectness(number, chars);
-		boolean result2 = checkIINMaskCorrectness (number, masks);
-		boolean result3 = checkLuhnalgorithmCorrectness(number);
-		
-		return result1 && result2 && result3;
+		List<Integer> chars = ccv.getNumberOfChars();
+		List<String> masks = ccv.getIIN();
+
+		return checkLengthCorrectness(number, chars) &&
+				checkIINMaskCorrectness (number, masks) &&
+				checkLuhnalgorithmCorrectness(number);
 	}
 	
-	private boolean checkLengthCorrectness(String number, ArrayList<Integer> chars){
-		boolean result = false;
-		for (int i = 0; i<chars.size();i++){
-			if (chars.get(i) == number.length())
-				result = true;
-		}
-		return result;
+	private boolean checkLengthCorrectness(String number, List<Integer> chars){
+		return chars.stream()
+				.anyMatch(aChar -> aChar == number.length());
 	}
 	
-	private boolean checkIINMaskCorrectness (String number, ArrayList<String> masks){
-		boolean result = false;
-		for (int i = 0; i<masks.size();i++){
-			if (number.startsWith(masks.get(i)))
-				result = true;
-		}
-		return result;
+	private boolean checkIINMaskCorrectness (String number, List<String> masks){
+		return masks.stream()
+				.anyMatch(number::startsWith);
 	}
 	
 	private boolean checkLuhnalgorithmCorrectness(String number){
-		boolean result = false;
-		
 		String[] characters = number.split("");
 		int[] intCharsReverse = new int[characters.length];
-		int sum = 0;
-		
-		for(int i = 0;i<characters.length;i++){
-			intCharsReverse[characters.length-1-i] = Integer.parseInt(characters[i]);
-		}
-		
-		for(int i = 0;i<intCharsReverse.length;i++){
-			intCharsReverse[i] = (i%2==1) ? 2*intCharsReverse[i] : intCharsReverse[i];
-		}
-		
-		for(int i = 0;i<intCharsReverse.length;i++){
-			sum += (intCharsReverse[i]<10) ? intCharsReverse[i] : (intCharsReverse[i]%10 + intCharsReverse[i]/10);
-		}
-		
-		if(sum%10==0)
-			result = true;
-		
-		return result;
+
+		IntStream.range(0, characters.length)
+				.forEach(i -> intCharsReverse[characters.length - 1 - i] = Integer.parseInt(characters[i]));
+
+		IntStream.range(0, intCharsReverse.length)
+				.forEach(i -> intCharsReverse[i] = (i % 2 == 1) ? 2 * intCharsReverse[i] : intCharsReverse[i]);
+
+		int sum = Arrays.stream(intCharsReverse)
+				.map(j -> (j < 10) ? j : (j % 10 + j / 10)).sum();
+
+		return sum % 10 == 0;
 	}
 
 }
