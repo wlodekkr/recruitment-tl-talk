5dbb167b1a92a53b8f601d7382997505522d0b78
Włodek Krakowski <wlodzimierz.krakowski@onet.pl>
2020-09-14 07:50:23 +0200

    Cleanup in CreditCardVendor : ArrayList -> List

--- src/main/java/validator/CreditCardVendor.java
+++ src/main/java/validator/CreditCardVendor.java
@@ -1,25 +1,25 @@
 package validator;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class CreditCardVendor {
 	private String name;
-	private ArrayList<Integer> numberOfChars;
-	private ArrayList<String> IIN;
+	private List<Integer> numberOfChars;
+	private List<String> IIN;
 	
 	public String getName(){
 		return name;
 	}
 	
-	public ArrayList<Integer> getNumberOfChars(){
+	public List<Integer> getNumberOfChars(){
 		return numberOfChars;
 	}
 	
-	public ArrayList<String> getIIN(){
+	public List<String> getIIN(){
 		return IIN;
 	}
 	
-	public CreditCardVendor(String vendorName, ArrayList<Integer> charsNumbers, ArrayList<String> masks){
+	public CreditCardVendor(String vendorName, List<Integer> charsNumbers, List<String> masks){
 		name = vendorName;
 		numberOfChars = charsNumbers;
 		IIN = masks;
